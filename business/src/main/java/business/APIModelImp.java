47035693f7e01f399c6c5a6892ac6f9ebcdba554
Sunny kumar <sunnygrg711@gmail.com>
2018-04-03 00:25:54 +0530

    Maven Release process demo

---
+++ business/src/main/java/business/APIModelImp.java
@@ -0,0 +1,10 @@
+package business;
+
+import api.APIModel;
+
+public class APIModelImp implements APIModel{
+
+	public static void  main(String ...strings) {
+		System.out.println("I am getting argement as " + strings[0] /*+ " and " + strings[1]*/ );
+	}
+}
